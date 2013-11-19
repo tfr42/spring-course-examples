8ded538ccb68a64c427e9e0cc6df6087d6ba2e50
Torsten <torsten.friebe@googlemail.com>
2013-11-19 23:29:24 +0100

    add mailer for objectmessages of type specialguest

--- lab/trainer/helloWorldSpringJMS/src/main/java/net/gfu/seminar/spring/helloworld/GreetingListener.java
+++ lab/trainer/helloWorldSpringJMS/src/main/java/net/gfu/seminar/spring/helloworld/GreetingListener.java
@@ -3,19 +3,31 @@ package net.gfu.seminar.spring.helloworld;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
 import javax.jms.TextMessage;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class GreetingListener implements MessageListener {
 	private static final Logger LOG = Logger.getLogger(GreetingListener.class);
 
+	@Autowired
+	private GreetingMailer mailService;
+
 	public void onMessage(Message message) {
 		if (message instanceof TextMessage) {
 			try {
-				LOG.info(((TextMessage) message).getText());
+				String text = ((TextMessage) message).getText();
+				LOG.info("Received message with text:" + text);
+
 			} catch (JMSException ex) {
 				throw new RuntimeException(ex);
+			}
+		} else if (message instanceof ObjectMessage) {
+			if (message instanceof SpecialGuest) {
+				SpecialGuest guest = (SpecialGuest) message;
+				mailService.sendMessage(guest);
 			}
 		} else {
 			throw new IllegalArgumentException(
