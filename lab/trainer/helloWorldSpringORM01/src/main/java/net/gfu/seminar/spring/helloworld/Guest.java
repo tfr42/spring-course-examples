05f6e25988b28244ae3cca72e2a1d716f44c1bf8
Torsten <torsten.friebe@googlemail.com>
2014-04-17 15:46:33 +0200

    Upgrade to Spring 3.2.8 and done major refactorings in helloWorldSpringORM01 (hibernate 4 support) and helloWorldSpringJSF01 (supporting JSF 2.1/2.2) with CDI. Furthermore cleaning up of Maven POM files.

--- lab/trainer/helloWorldSpringORM01/src/main/java/net/gfu/seminar/spring/helloworld/Guest.java
+++ lab/trainer/helloWorldSpringORM01/src/main/java/net/gfu/seminar/spring/helloworld/Guest.java
@@ -1,10 +1,91 @@
 package net.gfu.seminar.spring.helloworld;
 
+import java.io.Serializable;
+import java.util.Scanner;
 
-public interface Guest {
-	String getName();
-	void setName(String name);
-	String getLastName();
-	String getFirstName();
-	Long getId();
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("guest")
+@Entity @Table(name="guests")
+@NamedQueries({
+	@NamedQuery(name="forName", query="from GuestImpl as g where g.lastName like ?1"),
+	})
+public class GuestImpl implements Guest, Serializable  {
+	
+	private static final long serialVersionUID = 6697160661899899888L;
+	@Value("#{'Hans'}")
+	private String firstName;
+	@Value("#{'Fall'}")
+	private String lastName;
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	public GuestImpl() {
+		this("","");
+	}
+	
+	public GuestImpl(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+	public GuestImpl(Long id, String firstName, String lastName) {
+		this(firstName, lastName);
+		this.id = id;
+	}
+
+	@Override
+	public String getName() {
+		return this.firstName + " " + this.lastName;
+	}
+
+	@Override
+	public void setName(String name) {
+		Scanner scanner = new Scanner(name);
+		this.firstName = scanner.next();
+		this.lastName = scanner.next();
+	}
+
+	@Override
+	public String toString() {
+		return "GuestImpl [id=" + id + ", firstName=" + firstName
+				+ ", lastName=" + lastName + "]";
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 }
---
+++ lab/trainer/helloWorldSpringORM01/src/test/java/net/gfu/seminar/spring/helloworld/GreetingServiceTest.java
@@ -0,0 +1,39 @@
+package net.gfu.seminar.spring.helloworld;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/persistenceLayer.xml",
+		"classpath:/jdbcConfig.xml", "classpath:/applicationContext.xml" })
+public class GreetingServiceTest {
+
+	@Autowired
+	private DataSource dataSource;
+	@Autowired
+	private GreetingService service;
+	
+	@Before
+	public void setUp() throws Exception {
+		JdbcTemplate jt = new JdbcTemplate(dataSource);
+		jt.execute("INSERT INTO guests (firstname,lastname) VALUES ('Rainer','Fall')");
+	}
+
+	@Test
+	public void testWelcome() {
+		String message = service.welcome();
+		System.out.println(message);
+		assertThat(message, containsString("Fall"));
+	}
+
+}
