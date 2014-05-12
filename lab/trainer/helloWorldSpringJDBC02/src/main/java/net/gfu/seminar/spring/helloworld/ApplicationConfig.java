6437999a0d0364a224ccc7f43a57fd56d790a3bd
Torsten <torsten.friebe@googlemail.com>
2014-05-12 23:22:44 +0200

    set XSD version to 3.2

--- lab/trainer/helloWorldSpringJDBC02/src/main/java/net/gfu/seminar/spring/helloworld/ApplicationConfig.java
+++ lab/trainer/helloWorldSpringJDBC02/src/main/java/net/gfu/seminar/spring/helloworld/ApplicationConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @PropertySource("classpath:/guest.properties")
