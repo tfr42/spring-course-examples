920faa7c97eaaaf175116ea992c37d5a74c371ae
Torsten <torsten.friebe@googlemail.com>
2014-08-25 03:07:09 +0200

    Upgrade to Spring 4.0.6.RELEASE and all it dependencies

--- lab/trainer/helloWorldSpringORM01/src/main/java/net/gfu/seminar/spring/helloworld/GuestGenericDao.java
+++
@@ -1,7 +0,0 @@
-package net.gfu.seminar.spring.helloworld;
-
-import java.util.List;
-
-public interface GuestGenericDao extends GenericDao<Guest, Long> {
-	public List<Guest> findByName(String name);
-}
