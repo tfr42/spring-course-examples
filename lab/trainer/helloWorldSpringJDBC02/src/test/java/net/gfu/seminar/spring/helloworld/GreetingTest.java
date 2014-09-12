3b933d4c56149765b1de544f9be004f078dc9405
Torsten <torsten.friebe@googlemail.com>
2014-09-12 14:10:42 +0200

    align test with JDBC01

--- lab/trainer/helloWorldSpringJDBC02/src/test/java/net/gfu/seminar/spring/helloworld/GreetingTest.java
+++ lab/trainer/helloWorldSpringJDBC02/src/test/java/net/gfu/seminar/spring/helloworld/GreetingTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { ApplicationConfig.class})
+@ContextConfiguration(classes = { ApplicationConfig.class })
 public class GreetingTest {
 
 	private static final Logger LOG = Logger.getLogger(GreetingTest.class);
@@ -52,29 +52,27 @@ public class GreetingTest {
 	}
 
 	@Test
-	public void testGetGuest() {
-		assertNotNull(greeting.findAll());
-		//assertEquals("Hans Dampf", greeting.getGuest().getName());
-	}
-	
-	@Test public void testCreateNewGuest() {
+	public void testCreateNewGuest() {
 		this.greeting.addGuest(new GuestImpl("Hans", "Dampf"));
 		assertNotNull(greeting.findByName("Hans", "Dampf"));
 	}
 	
-	@Test public void testFindAll() { 
+	@Test
+	public void testFindAll() { 
 		List<Guest> all = this.greeting.findAll();
 		assertFalse(all.isEmpty());
 	}
 
-	@Test public void testFindByName() {
+	@Test
+	public void testFindByName() {
 		String firstname = "Hans";
 		String lastname = "Dampf";
 		Guest guest = this.greeting.findByName(firstname, lastname);
 		assertNotNull(guest);
 	}
 
-	@Test public void testFindById() {
+	@Test
+	public void testFindById() {
 		Long id = Long.valueOf(1l);
 		Guest guest = this.greeting.findById(id);
 		assertNotNull(guest);
