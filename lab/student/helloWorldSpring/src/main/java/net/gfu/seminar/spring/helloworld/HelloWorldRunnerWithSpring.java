c727d3660c1376065c630bbb028795ad9fbed3b3
Torsten <torsten.friebe@googlemail.com>
2014-09-12 14:07:15 +0200

    avoid casting

--- lab/student/helloWorldSpring/src/main/java/net/gfu/seminar/spring/helloworld/HelloWorldRunnerWithSpring.java
+++ lab/student/helloWorldSpring/src/main/java/net/gfu/seminar/spring/helloworld/HelloWorldRunnerWithSpring.java
@@ -18,14 +18,14 @@ public class HelloWorldRunnerWithSpring {
 		// 1. The Bean Factory
 		// loading the Spring XML configuration file and initializing the BeanFactory
 		// this file is located in the classpath. 
-		// TODO Open the Eclipse Project properties and check the Java Build Path
+		// TODO Open in your IDE the project settings and check the Java Build Path
 		Resource resource = new ClassPathResource("applicationContext.xml"); //located in src/main/resources
 		
 		// this creates a new instance of the Spring Container aka BeanFactory
 		BeanFactory beanFactory = new XmlBeanFactory(resource); 
 		
 		// here we retrieve an instance of Greeting from the Spring BeanFactory
-		Greeting greeting = (Greeting) beanFactory.getBean("greeting");
+		Greeting greeting = beanFactory.getBean("greeting", Greeting.class);
 		
 		System.out.println(greeting.welcome());
 	}
