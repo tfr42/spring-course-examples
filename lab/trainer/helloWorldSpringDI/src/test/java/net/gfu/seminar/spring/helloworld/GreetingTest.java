dad0c134a0ece91f60f71193405a85679b46ec53
Torsten Friebe <torsten.friebe@gmail.com>
2016-12-02 00:41:27 +0100

    Disabled ContextConfiguration Loader

--- lab/trainer/helloWorldSpringDI/src/test/java/net/gfu/seminar/spring/helloworld/GreetingTest.java
+++ lab/trainer/helloWorldSpringDI/src/test/java/net/gfu/seminar/spring/helloworld/GreetingTest.java
@@ -12,8 +12,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
 public class GreetingTest {
 
 	private GreetingService service;
