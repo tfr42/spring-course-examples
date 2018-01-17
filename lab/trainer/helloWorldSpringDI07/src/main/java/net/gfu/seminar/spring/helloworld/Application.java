4a9c4d660c89cb71060bf91268351a5427a792e8
Torsten Friebe <torsten.friebe@gmail.com>
2018-01-17 22:53:02 +0100

    Renamed HelloWorldRunner to Application in trainer/helloWorldSpringDI07

--- lab/trainer/helloWorldSpringDI07/src/main/java/net/gfu/seminar/spring/helloworld/Application.java
+++ lab/trainer/helloWorldSpringDI07/src/main/java/net/gfu/seminar/spring/helloworld/Application.java
@@ -4,25 +4,23 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * HelloWorld using Spring Framework Dependency Injection.
+ * Starts the HelloWorld application using Spring Framework BeanFactory.
  * 
  * @author tf
  * @see <a href="http://docs.spring.io/spring/docs/current/spring-framework-reference/html/beans.html#beans-introduction">Spring DI</a>
  */
-public class HelloWorldRunnerWithSpring {
+public class Application {
 
 	public static void main(String[] args) {
-		// loading the Spring XML configuration file and initializing the ApplicationContext
+		// Loads the Spring XML configuration files and initializing the ApplicationContext
 		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		// here we retrieve an instance of Greeting from the Spring Container
+		// Retrieves an instance of Greeting from the Spring Container
 		Greeting greeting = context.getBean("greeting", Greeting.class);
 
 		System.out.println(greeting.welcome());
 
 		context.registerShutdownHook();
 		context.close();
-
 	}
-
 }
