20c28a0a28eb138b147c5075a2997ef7eafc682c
Torsten Friebe <torsten.friebe@gmail.com>
2018-01-17 22:53:37 +0100

    Renamed HelloWorldRunner to Application in trainer/helloWorldSpringDI08

--- lab/trainer/helloWorldSpringDI08/src/main/java/net/gfu/seminar/spring/helloworld/Application.java
+++ lab/trainer/helloWorldSpringDI08/src/main/java/net/gfu/seminar/spring/helloworld/Application.java
@@ -4,20 +4,22 @@ import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 import org.springframework.context.support.GenericApplicationContext;
 
 /**
- * HelloWorld using Spring Framework Dependency Injection and Annotations as configuration metadata.
+ * Starts the HelloWorld application using Spring Framework Dependency Injection and Annotations as configuration metadata.
+ *
  * @author tf
  * @see <a href="http://docs.spring.io/spring/docs/current/spring-framework-reference/html/beans.html#beans-java">Java-based container configuration</a>
  */
-public class HelloWorldRunnerWithSpringAndAnnotation {
+public class Application {
+
 	public static void main(String[] args) {
-		// creates new ApplicationContext
+		// Creates new ApplicationContext
 		GenericApplicationContext context = new GenericApplicationContext();
 		
-		// scans for Spring Beans annotated with @Component or @Configuration
+		// Scans for Spring Beans annotated with @Component or @Configuration
 		new ClassPathBeanDefinitionScanner(context)
 				.scan("net.gfu.seminar.spring.helloworld");
 		
-		// creates all bean instances
+		// Creates all bean instances
 		context.refresh();
 
 		Greeting greeting = context.getBean("greeting", Greeting.class);
